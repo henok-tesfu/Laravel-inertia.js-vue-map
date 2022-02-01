aa60dd189b3e702188737fa877131ced5ae3a846
henok-tesfu <henoktesfu48@gmail.com>
2022-02-01 11:31:52 +0300

    tailwind init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,8 @@
 import Vue from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'YOUR_GOOGLE_MAPS_API_KEY_GOES_HERE',
-    libraries: 'places',
-  }
-});
+import { InertiaProgress } from '@inertiajs/progress'
+
 
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
@@ -17,4 +13,24 @@ createInertiaApp({
       render: h => h(App, props),
     }).$mount(el)
   },
+});
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'YOUR_GOOGLE_MAPS_API_KEY_GOES_HERE',
+    libraries: 'places',
+  }
+});
+InertiaProgress.init({
+  // The delay after which the progress bar will
+  // appear during navigation, in milliseconds.
+  delay: 250,
+
+  // The color of the progress bar.
+  color: '#29d',
+
+  // Whether to include the default NProgress styles.
+  includeCSS: true,
+
+  // Whether the NProgress spinner will be shown.
+  showSpinner: false,
 });
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./resources/js/pages/*.vue",
+    "./resources/js/pages/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
